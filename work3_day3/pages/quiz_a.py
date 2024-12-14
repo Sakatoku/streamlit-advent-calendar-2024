24f86a224d58c5deb16b218501dc8867c0ec5f7f
mrks <haruki.mrks@gmail.com>
2024-12-14 09:37:10 +0900

    14日目の記事追加、8日目へ名前追加

--- work3_day3/pages/quiz_a.py
+++ work3_day3/pages/quiz_a.py
@@ -9,12 +9,38 @@ st.write(
     "クイズA：以下のコードを実行すると何が表示されますか？"
 )  # ここはCSSとかでちょっといい感じに表示できると良いかも？(Day3で対応したい)
 
+# st.html('''
+#         <!-- CSSでスタイルを設定 -->
+#         <style>
+#         .quiz-text{
+#             padding: 0.5em 1em;
+#             margin: 2em 0;
+#             color: #474747;
+#             background: whitesmoke;/*背景色*/
+#             border-left: double 7px #4ec4d3;/*左線*/
+#             border-right: double 7px #4ec4d3;/*右線*/
+#         }
+#         .quiz-text p {
+#             margin: 0; 
+#             padding: 0;
+#             font-size: 1.3rem;
+#             font-weight: bold;
+#         }
+#         </style>
+
+#         <!-- クイズ文言の表示 -->
+#         <div class="quiz-text">
+#             <p>クイズA：以下のコードを実行すると何が表示されますか？</p>
+#         </div>
+#         ''')
+
 st.code(
-    """
+"""
 import streamlit as st
 
 st.write("Streamlitはとても簡単です！")
 """
+,language="python", line_numbers=True
 )
 
 options = ["エラーが出る", "Streamlitはとても簡単です！", "何も表示されない"]
