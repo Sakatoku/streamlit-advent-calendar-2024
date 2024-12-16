625c97f2f7c87e941868fa2af371fa1ede800515
THiyama <robot_toru@outlook.jp>
2024-12-17 00:12:17 +0900

    add day20.md

--- work4_day1.py
+++ work4_day1.py
@@ -1,21 +1,11 @@
 import streamlit as st
 from snowflake.snowpark.context import get_active_session
 
-
 # Get the current credentials
 session = get_active_session()
 
-# Day1
 
-"""書きたいことのメモ
-今回使うライブラリは、Snowflake-ml-pythonライブラリの1.6.4。
-https://docs.snowflake.com/en/developer-guide/snowpark-ml/reference/1.6.4/api/model/snowflake.cortex.Complete
-
-
-・パッケージの読み込み方法を解説。
-・Cortex LLMを使ってみる
-"""
-
+# Snowflake LLMとの対話
 from snowflake.cortex import Complete
 
 request = st.text_input("プロンプトを入力してください", key="complete")
@@ -23,6 +13,7 @@ response = Complete("mistral-large2", request)
 st.write(response)
 
 
+# Snowflake LLMによる翻訳
 from snowflake.cortex import Translate
 
 request = st.text_input("プロンプトを入力してください", key="translate")
@@ -30,10 +21,9 @@ response = Translate(request, "ja", "en")
 st.write(response)
 
 
+# Snowflake LLMによる感情分析
+from snowflake.cortex import Translate, Sentiment
 
-"""書きたいことのメモ
-他にも様々な関数があるので、遊んでみてね。
-https://docs.snowflake.com/en/user-guide/snowflake-cortex/llm-functions#task-specific-functions
-https://docs.snowflake.com/en/developer-guide/snowpark-ml/reference/1.6.4/index
-
-"""
+request = st.text_input("プロンプトを入力してください")
+response = Sentiment(Translate(request, "ja", "en"))
+st.write(response)
