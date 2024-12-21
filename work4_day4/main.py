03906fd762f1c4f1cecc3ccd59a67975df034657
THiyama <robot_toru@outlook.jp>
2024-12-21 20:20:10 +0900

    add day23.md

--- work4_day4/main.py
+++ work4_day4/main.py
@@ -1,6 +1,15 @@
 import streamlit as st
 
+# API Key
 st.write(f"your sample API key: {st.secrets['API_Key']}")
 
+# Group User & Pass
+st.write(f"your username: {st.secrets['group1']['username']}")
+st.write(f"your password: {st.secrets['group1']['password']}")
+
+# st.connectionによるSnowflakeセッションの作成
 session = st.connection("snowflake").session()
-st.write(session.sql("select current_version()").collect()[0][0])
+st.write(session.sql("select current_user()").collect()[0][0])
+
+# st.text_inputによるパスワードの取得
+password = st.text_input("パスワードを入力してください", type="password")
